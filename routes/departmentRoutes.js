5dc0a9f0e10475955a74d1a470c500ebb37d507c
om patel <omshan.patel@gmail.com>
2023-01-12 18:27:19 +0530

    NodeJs

--- routes/departmentRoutes.js
+++ routes/departmentRoutes.js
@@ -1,7 +1,7 @@
 const express=require('express');
-const departmentController=require('../controller/departmentController');
+const departmentController1=require('../Controller/DepartmentController');
 const routes = express.Router();
 
-routes.post('/department',departmentController.createDepartment)
-routes.put('/department/:id',departmentController.updateEmployees)
+routes.post('/department',departmentController1.createDepartment)
+routes.put('/department/:id',departmentController1.updateEmployees)
 module.exports=routes;
